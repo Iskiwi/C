11f340e449c86f76db0dee011219fe20a4311203
Xiao Yi <2102004995@qq.com>
2022-08-17 20:33:31 +0800

    文件的读写、顺序写入、输入流和输出流

---
+++ EveryDay/day22_8_16/Day_8_16/FILE_Format_data.c
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+//
+//int main()
+//{
+//	FILE* pf = fopen("fscanf_fprintf.txt", "w");
+//	//位运算符 & | ~ ^ << >> 
+//	//逻辑运算符 && || ！
+//	if (!pf)
+//	{
+//		perror("fopen");
+//		
+//		//fputs(stderr, stdout);//不能打印出错误码
+//		fputs(fgetc(stderr), stdout);
+//		printf("%s", strerror(errno));
+//		return 1;
+//		//没有效果，不知道这样是否有问题
+//	}
+//	//操作文件
+//	fprintf(stdout, "%s %d", "fprintf", 12);
+//	fprintf(pf, "%s %d", "fscanf_get_format_str", 8.17);
+//
+//	//关闭文件
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//	FILE* pf = fopen("fscanf_fprintf.txt", "w");
+//	FILE* rf = fopen("testfile.txt", 'r');
+//	if (!pf && !rf)
+//	{
+//		perror("fopen");
+//
+//		/*fputs(fgetc(stderr), stdout);
+//		printf("%s", strerror(errno));*/
+//		return 1;
+//		//没有效果，不知道这样是否有问题
+//	}
+//	//操作文件
+//	fprintf(stdout, "%s %d", "fprintf", 12);
+//	fprintf(pf, "%s %lf", "fscanf_get_format_str", 8.17);
+//
+//	////通过使用fscanf获取输入
+//	//char text[100] = { "afsdfdsfadadsf" };
+//	//fscanf(rf, "%s", text);
+//	//printf("%s", text);
+//	//关闭文件
+//	fclose(pf);
+//	pf = NULL;
+//	fclose(rf);
+//	rf = NULL;
+//	return 0;
+//}
+
+//输入流和输出流
---
+++ EveryDay/day22_8_16/Day_8_16/error.c
@@ -0,0 +1,76 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <errors.h>
+#include <string.h>
+
+// errorno
+// strerror
+ //perror
+
+int main()
+{
+	// 打开文件
+	FILE* pf = fopen("testfile.txt", "r");
+	if (NULL == pf)
+	{
+		perror("fopen");
+		//strerror(errno);
+		return 1;
+	}
+	//文件操作
+	//fgetc(pf);
+	//
+	//fgetc(pf);
+	////printf("%c", fgetc(pf));
+
+	//char arr[10] = { 0 };
+	//fgets(arr, 3, pf);
+	//printf("%s", arr);  //cd
+	//printf("%s", fgets(arr, 3, pf));  //cd
+	
+	char arr[10] = { 0 };
+	fgets(arr, 5, pf);
+	fputs("hello world\n", stdout);
+	printf("%d",fputs(arr, stdout));
+
+	//fgets()，有三个参数,存放n个字符的位置，n,输入流
+	//返回值是存放n个字符的char*
+
+	//fputs()，string，FILE* stream, return value:int，if return error,return EOF(-1)
+
+	//文件关闭
+	fclose(pf);
+	pf = NULL;
+	return 0;
+}
+//
+//int main()
+//{
+//	// 打开文件
+//	FILE* pf = fopen("testfile.txt", "w");
+//	if (NULL == pf)
+//	{
+//		perror("fopen");
+//		return 1;
+//	}
+//	//文件操作
+//	//完成一个从键盘标准输入流获取数据，再写入文件
+//	char arr[10] = { 0 };
+//	fgets(arr, 10, stdin);
+//
+//	/*FILE* pff = NULL;
+//	if (fputs(arr, pff) != EOF)
+//	{
+//		NULL;
+//	}
+//	else
+//	{
+//		perror("fputs");
+//	}*/
+//	fputs(arr, pf);
+//	
+//	//文件关闭
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
